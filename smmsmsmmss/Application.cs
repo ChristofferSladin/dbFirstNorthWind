9f72f8f429b56f88980ad38b8e74ade6d9538bf4
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2022-12-07 21:25:52 +0100

    Add project files.

---
+++ smmsmsmmss/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
+using smmsmsmmss.Models;
+
+public class Application
+{
+    public void Run()
+    {
+        using (var dbContext = new NorthwindContext())
+        {
+            foreach (var user in dbContext.Customers.Where(u => u.ContactName.StartsWith("A")).OrderBy(u => u.City))
+            {
+                Console.WriteLine($"{user.CustomerId}: {user.City} - {user.ContactName} -- {user.Country}");
+            }
+
+
+        }
+    }
+}
